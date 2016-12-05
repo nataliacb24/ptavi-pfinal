2e8903acbb7828d2eb638ca8b502bca5412bd1d2
nataliacb24 <natalia-cb@live.com>
2016-12-05 18:10:19 +0100

    Practica final a medias server y client

--- uaclient.py
+++ uaclient.py
@@ -3,62 +3,117 @@
 
 import socket
 import sys
+import os
+
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 
 if len(sys.argv) == 4:
     config = sys.argv[1]
-    method = sys.argv[2]
+    metodo = sys.argv[2]
     option = sys.argv[3]
 else:
     sys.exit("Usage: python uaclient.py config method option")
 
-class XMLHandler(Contenthandler):
+
+class FicheroXML(ContentHandler):
+    """
+    Se crea el fichero XML
+    """
 
     def __init__(self):
-        self.ListaDicc = []
-        self.DiccAtrib = {'account':['username', 'passwwd'],
-                          'uaserver':['ip', 'puerto'],
-                          'rtpaudio':['puerto'],
-                          'regproxy':['ip', 'puerto'],
-                          'log':['path'],
-                          'audio':['path']}
-    
-    def startElement(self, name, attrs):
-    
-        dicc = {}
-        dicc = {'Tag' = name}
-        for atribute in self.DiccAtrib[name]
-            dicc[atribute] =  attrs.get(atribute, "")
-        self.ListaDicc.append()
 
+        self.ListaDicc = []
+        self.DiccAtrib = {'account': ['username', 'passwd'],
+                          'uaserver': ['ip', 'puerto'],
+                          'rtpaudio': ['puerto'],
+                          'regproxy': ['ip', 'puerto'],
+                          'log': ['path'],
+                          'audio': ['path']}
+
+    def startElement(self, name, attrs):
+
+        dicc = {}
+        # si existe la etiqueta en mi dicc
+        if name in self.DiccAtrib:
+            dicc = {'Tag': name}
+            # recorre los atributos y se guardan en el dicc de tags
+            for atribute in self.DiccAtrib[name]:
+                dicc[atribute] = attrs.get(atribute, "")
+            # guarda sin sustituir lo que ya habia dentro
+            self.ListaDicc.append(dicc)
+
+    def get_tags(self):
+        # Metodo que me imprime la lista de diccionarios de tags
+        return self.ListaDicc
+
+# parseamos y separamos el fichero para poder trabajar con él
+parser = make_parser()
+XMLHandler = FicheroXML()
+parser.setContentHandler(XMLHandler)
+parser.parse(open(config))
+list_XML = XMLHandler.get_tags()
+# print(list_XML)
+username = list_XML[0]['username']
+ua_ip = list_XML[1]['ip']
+ua_port = list_XML[1]['puerto']
+audiortp_port = list_XML[2]['puerto']
+proxy_ip = list_XML[3]['ip']
+proxy_port = list_XML[3]['puerto']
+fichero_audio = list_XML[5]['path']
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((IP, port))
+my_socket.connect((ua_ip, int(ua_port)))
 
-Peticion = config + " " + method + " " + option
-my_socket.send(bytes(Peticion, 'utf-8') + b'\r\n')
+# def log
+
+# Si recibimos una cosa u otra
+if metodo == 'REGISTER':
+    Peticion = metodo + " sip:" + username + ":" + ua_port
+    Peticion += " SIP/2.0" + b"\r\n" + "Expires: " + option + "\r\n"
+elif metodo == 'INVITE':
+    Peticion = metodo + " sip:" + option + " SIP/2.0 \r\n\r\n"
+    Peticion += "Content-Type: application/sdp \r\n"
+    Peticion += "v=0 \r\n" + "o=" + username + " " + ua_ip + "\r\n"
+    Peticion += "s=misesion \r\n" + "t=0 \r\n"
+    Peticion += "m=audio " + audiortp_port + " RTP \r\n\r\n"
+elif metodo == 'BYE':
+    Peticion = metodo + " sip:" + option + " SIP/2.0 \r\n\r\n"
+
+
+# hora y lo que pasa en el log: enviando desde..
+# log
+print("Enviando: ", Peticion)
+my_socket.send(bytes(Peticion, 'utf-8') + b'\r\n\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
-datos = data.decode('utf-8').split()
-if datos[1] == "100" and datos[4] == "180" and datos[7] == "200":
-    Peticion = "ACK sip:" + option + "@" + IP + " SIP/2.0" + "\r\n"
-    my_socket.send(bytes(Peticion, 'utf-8') + b'\r\n')
-print("Terminando socket...")
+datos_rcv = data.decode('utf-8').split()
+# hora y lo que pasa en el log: recibido desde..
+# log
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((IP, int(PORT)))
-    if METODO == "register":
-        PET = "REGISTER sip:" + address + ":" + port + " SIP/2.0 \r\n" + " Expires: " + option
-    print("Enviando:", '\n', PET)
-    my_socket.send(bytes(PET, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+
+# if metodo == "REGISTER":
+if metodo == "INVITE":
+    if datos_rcv[1] == "100" and datos_rcv[4] == "180" and datos_rcv[7] == "200":
+        metodo = "ACK"
+        Ip_Serv = datos_rcv[13]
+        Port_Serv = datos_rcv[17]
+        Peticion = metodo + " sip: " + option + " SIP/2.0 \r\n"
+        print("Enviando: ", Peticion)
+        my_socket.send(bytes(Peticion, 'utf-8') + b'\r\n')
+        aEjecutar = "mp32rtp -i " + Ip_Serv + " -p " + Port_Serv + ' < ' + fichero_audio
+        print("Vamos a ejecutar", aEjecutar)
+        os.system(aEjecutar)
+        # hora y lo que pasa en el log: enviando desde..
+        # log
 
 # Cerramos todo
+print("terminando socket...")
 my_socket.close()
 print("Fin.")
+# hora y lo que pasa en el log
+# log
--- uaserver.py
+++ uaserver.py
@@ -1,60 +1,126 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+
 import socketserver
 import sys
+import os
+
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
 
-class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
+if len(sys.argv) == 2:
+    config = sys.argv[1]
+else:
+    sys.exit("Usage: python server.py config")
 
-    def handler(self):
+
+class FicheroXML(ContentHandler):
+
+    def __init__(self):
+
+        self.ListaDicc = []
+        self.DiccAtrib = {'account': ['username', 'passwd'],
+                          'uaserver': ['ip', 'puerto'],
+                          'rtpaudio': ['puerto'],
+                          'regproxy': ['ip', 'puerto'],
+                          'log': ['path'],
+                          'audio': ['path']}
+
+
+    def startElement(self, name, attrs):
+
+        dicc = {}
+        # si existe la etiqueta en mi dicc
+        if name in self.DiccAtrib:
+            dicc = {'Tag': name}
+            # recorre los atributos y se guardan en el dicc de tags
+            for atribute in self.DiccAtrib[name]:
+                dicc[atribute] = attrs.get(atribute, "")
+            # guarda sin sustituir lo que ya habia dentro
+            self.ListaDicc.append(dicc)
+
+    def get_tags(self):
+        # Metodo que me imprime la lista de diccionarios de tag
+        return self.ListaDicc
+
+# Parseamos y separamos el fichero en variables para poder trabajar con él
+parser = make_parser()
+XMLHandler = FicheroXML()
+parser.setContentHandler(XMLHandler)
+parser.parse(open(config))
+# XML a mi dicc
+list_XML = XMLHandler.get_tags()
+#print(list_XML)
+username = list_XML[0]['username']
+ua_ip = list_XML[1]['ip']
+ua_port = list_XML[1]['puerto']
+audiortp_port = list_XML[2]['puerto']
+proxy_ip = list_XML[3]['ip']
+proxy_port = list_XML[3]['puerto']
+fichero_audio = list_XML[5]['path']
+
+#def log
+
+
+class ServerHandler(socketserver.DatagramRequestHandler):
+
+    dicc_rtp = {'Ip_Client':'', 'Port_CLient': 0}
+
+    def handle(self):
+
+        IP_Client = self.client_address[0]
+        Port_Client = self.client_address[1]
+        print("IP: ", IP_Client)
+        print("Port: ", Port_Client)
         while 1:
-        # leyendo linea a linea lo que nos envia el cliente
-        Ip_client = self.cient_address[0]
-        line = self.rfile.read()
-        Peticion = line.decode('utf-8')
-        print("El cliente nos manda " + Peticion)
-        if not Peticion_SIP:
-             break
-        (metodo, address, sip) = Peticion.split()
-            if metodo != 'REGISTER' and not "@" in address:
+            # Leyendo línea a línea lo que nos envía el cliente
+            line = self.rfile.read()
+            petc_meth = line.decode('utf-8')
+            if not petc_meth:
                 break
-            time_now = int(time.time()) + int(expire)
-            time_gm = time.gmtime(time_now)
-            time_exp = time.strftime('%Y-%m-%d %H:%M:%S', time_gm)
-            if expire == '0':
-                del self.Dicc[address]
+            print("El cliente nos manda: " + petc_meth)
+            lista = petc_meth.split()
+            #print(lista)
+            metodo = lista[0]
+            if metodo not in ["INVITE", "BYE", "ACK"]:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed" + b"\r\n\r\n")
+                # hora y lo que ha pasado en log: enviando a
+                # log
+            elif metodo == "INVITE":
+                self.dicc_rtp['Ip_Client'] = lista[7]
+                self.dicc_rtp['Port_Client'] = lista[11]
+                self.wfile.write(b"SIP/2.0 100 Trying" + b"\r\n\r\n")
+                self.wfile.write(b"SIP/2.0 180 Ring" + b"\r\n\r\n")
+                Peticion = "SIP/2.0 200 OK \r\n\r\n"
+                Peticion += "Content-Type: application/sdp \r\n"
+                Peticion += "v=0 \r\n" + "o=" + username + " "
+                Peticion += ua_ip + "\r\n" + "s=misesion \r\n" + "t=0 \r\n"
+                Peticion += "m=audio " + str(audiortp_port) + " RTP \r\n\r\n"
+                self.wfile.write(bytes(Peticion, 'utf-8'))
+                # hora y lo que ha pasado en log: enviando a
+                # log
+            elif metodo == "ACK":
+                # print(self.dicc_rtp['Ip_Client'])
+                # print(self.dicc_rtp['Port_Client'])
+                aEjecutar = "mp32rtp -i " + self.dicc_rtp['Ip_Client'] + " -p "
+                aEjecutar += self.dicc_rtp['Port_Client'] + ' < ' + fichero_audio
+                print("Vamos a ejecutar", aEjecutar)
+                os.system(aEjecutar)
+            elif metodo == "BYE":
+                self.wfile.write(b"SIP/2.0 200 OK" + b"\r\n\r\n")
+                # hora y lo que ha pasado en log: enviando a
+                # log
             else:
-                self.Dicc[address] = [str(IP_client), str(time_exp)]
-            self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-            self.delete()
-
-        if metodo not in ["INVITE", "BYE", "ACK"]:
-            self.wfile.write(b"SIP/2.0 405 Method Not Allowed" + b"\r\n" + b"\r\n")
-        elif metodo == "INVITE":
-            self.wfile.write(b"SIP/2.0 100 Trying" + b"\r\n" + b"\r\n")
-            self.wfile.write(b"SIP/2.0 180 Ring" + b"\r\n" + b"\r\n")
-            self.wfile.write(b"SIP/2.0 200 OK" + b"\r\n" + b"\r\n")
-        elif metodo == "ACK":
-            aEjecutar = "mp32rtp -i " + IP + " -p 23032 < " + fichero_audio
-            print("Vamos a ejecutar", aEjecutar)
-            os.system(aEjecutar)
-        elif metodo == "BYE":
-            self.wfile.write(b"SIP/2.0 200 OK" + b"\r\n" + b"\r\n")
-        else:
-            self.wfile.write(b"SIP/2.0 400 Bad Request" + b"\r\n" + b"\r\n")
+                self.wfile.write(b"SIP/2.0 400 Bad Request" + b"\r\n\r\n")
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 2:
-        config = sys.argv[1]
-        Port_serv = '5555'
-        serv = socketserver.UDPServer(('', Port_serv), EchoHandler)
-        print("Listening")
-        serv.serve_forever()
-    else:
-        sys.exit("Usage:python uaserver.py config")
+
+    serv = socketserver.UDPServer((ua_ip, int(ua_port)), ServerHandler)
+    print("Listening")
+    # hora y lo que pasa en log
+    # log
+    serv.serve_forever()
